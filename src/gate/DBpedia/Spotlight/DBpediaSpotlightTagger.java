e78c713d352563ff6a923233c2b5312680127ede
Jenda Rybak <jendarybak@gmail.com>
2013-01-24 20:55:45 +0100

    Communication with DBpedia Spotlight
    Sending POST request to endpoint and receiving answer.

--- src/gate/DBpedia/Spotlight/DBpediaSpotlightTagger.java
+++ src/gate/DBpedia/Spotlight/DBpediaSpotlightTagger.java
@@ -21,6 +21,10 @@
 
 package gate.DBpedia.Spotlight;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.*;
 import java.util.ArrayList;
 
@@ -29,9 +33,18 @@ import gate.creole.*;
 import gate.creole.metadata.*;
 import gate.util.*;
 
-
 /** 
- * This class is the implementation of the resource DBPEDIA SPOTLIGHT.
+ * <p>A named entity annotator using DBpedia Spotlight endpoint.
+ *  DBpedia Spotlight is called using its REST interface, via a 
+ *  http POST.</p> 
+ *  
+ *  <p>This GATE Processing resource was written as a part of
+ *  Jan Rybak's Master's thesis at the University of West Bohemia, 
+ *  Czech Rep. and Norwegian University of Science and Technology.</p>
+ * 
+ *  @version 0.1  
+ *  @author jendarybak@gmail.com
+ *  @see http://github.com/jendarybak/GATE-DBpedia_Spotlight
  */
 @CreoleResource(name = "DBpedia Spotlight", comment = "DBpedia Spotlight Entity Recognizer")
 public class DBpediaSpotlightTagger extends AbstractLanguageAnalyser
@@ -64,12 +77,18 @@ public class DBpediaSpotlightTagger extends AbstractLanguageAnalyser
   public Resource init() throws ResourceInstantiationException {
     
     // DBpedia Spotlight URL is mandatory
-    if(dbpediaUrlString == null) {
+    if (dbpediaUrlString == null || dbpediaUrlString.trim().length() == 0) {
       throw new ResourceInstantiationException("DBpedia Spotlight URL is missing!");
     }
     
     // init dbpedia url and try if it can be connected
     try {
+      dbpediaUrlString = dbpediaUrlString.trim();
+      
+      if (!dbpediaUrlString.endsWith("/")) {
+    	  dbpediaUrlString += "/";
+      }
+      
       dbpediaUrl = new URL(dbpediaUrlString);
     }
     catch(MalformedURLException e) {
@@ -118,9 +137,13 @@ public class DBpediaSpotlightTagger extends AbstractLanguageAnalyser
             .length() == 0) ? document.getAnnotations() : document
             .getAnnotations(outputASName);
 
-
     try {
+      // document in plain text
       String docText = document.getContent().toString();
+      
+      // TODO: just testing
+      // load response from DBpedia Spotlight server 
+      String DBpediaSpotlightResponse = receiveEntities(docText);
       
       //TODO: load entities (remote request)
       
@@ -140,14 +163,90 @@ public class DBpediaSpotlightTagger extends AbstractLanguageAnalyser
           FeatureMap fm = gate.Factory.newFeatureMap();
           outputAs.add(new Long(ent.getStart()), new Long(ent.getEnd()), "NER", fm); // TODO: set customised name of output
         }
-    }
-    catch(InvalidOffsetException e) {
+      
+    } catch(InvalidOffsetException e) {
       throw new ExecutionException(e);
     }
 
     // process is done, nice!
     fireProcessFinished();
   }  
+  
+  
+  /**
+   * Sends request (plain text) to DBpedia Spotlight endpoint and  
+   * receives response with annotated text.<BR>
+   * 
+   * @param	documentText	plain text document	
+   * @return 				annotated text in XML format
+   */  
+  private String receiveEntities(String documentText) {
+  	
+	// settings
+	HttpURLConnection connection = null;
+  	final String charset = "UTF-8";
+  	final String method = "POST";
+  	final String contentType = "application/x-www-form-urlencoded";
+  	final String accept = "text/xml";
+  	final String language = "en-US";
+		
+	try {
+	    //Create connection
+		String urlParameters =
+		        "text=" + URLEncoder.encode(documentText, charset) +
+		        "&confidence=" + URLEncoder.encode(Double.toString(confidence), charset) +
+		        "&support=" + URLEncoder.encode(Double.toString(support), charset) +
+		        "&disambiguator=" + URLEncoder.encode("Document", charset);
+		
+	    connection = (HttpURLConnection) dbpediaUrl.openConnection();
+	    connection.setRequestMethod(method);
+	    connection.setRequestProperty("Content-Type", contentType);
+	    connection.setRequestProperty("Accept", accept);
+	    //connection.setReadTimeout(10000);
+	    //connection.setConnectTimeout(15000);
+				
+	    connection.setRequestProperty("Content-Length", "" + 
+	               Integer.toString(urlParameters.getBytes().length));
+	    connection.setRequestProperty("Content-Language", language);  
+				
+	    connection.setUseCaches (false);
+	    connection.setDoInput(true);
+	    connection.setDoOutput(true);
+
+	    //Send request
+	    DataOutputStream wr = new DataOutputStream (
+	                  connection.getOutputStream ());
+	    wr.writeBytes (urlParameters);
+	    wr.flush ();
+	    wr.close ();
+	    
+
+	    //Get Response	
+	    InputStream is = connection.getInputStream();
+	    BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+	    String line;
+	    StringBuffer response = new StringBuffer(); 
+	    
+	    while((line = rd.readLine()) != null) {
+	    	response.append(line);
+	    	response.append('\r');
+	    }
+	    
+	    rd.close();
+	    
+	    // return response
+	    return response.toString();
+
+	} catch (Exception e) {
+		e.printStackTrace();
+		throw new GateRuntimeException("Error during communication with DBpedia Spotlight server!");
+
+	} finally {
+		if (connection != null) {
+			connection.disconnect(); 
+	    }
+	}
+  }
   
   /** 
    * Getters and setters
